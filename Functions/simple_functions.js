d0e376a99e0d3c3f50f20e0fc45f583a17e66fac
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-01 11:31:13 -0400

    exercise 1

---
+++ Functions/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function favoriteThings(){
+    console.log("Name - Yonesh Thapa");
+    console.log("Favorite Movie - Avengers Infinity War");
+    console.log("Favorite Band - N/A");
+}
+
+function whyImHere(){
+    console.log("I joined Learning to Code to get better at coding and to get a job");
+
+}
+
+function favoritePlace(){
+    console.log("Vernon Jackson is my favorite place. It has a beautiful sunset and and great night view of Manhattan skyscrappers.");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
+
