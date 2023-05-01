da98b10b6d4c096ba877d2da8667e83cc10d122b
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-01 19:41:02 -0400

    parsing and displaying name

---
+++ Strings/parsing.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function parseAndDisplayName(name){
+let space = name.indexOf(" ");
+let first = name.substring(0,space);
+let last = name.substring(space + 1);
+
+console.log(`Name: ${name}
+First name: ${first}
+Last name: ${last}`);
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
