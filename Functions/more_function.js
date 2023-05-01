2b5c1e39e7f164fc6254dabdbf28840d5c0fa8fb
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-01 13:37:38 -0400

    updated way of writing code

--- Functions/more_function.js
+++ Functions/more_function.js
@@ -1,7 +1,12 @@
 function displayMailingLabel(name, address, city, state, zip) {
-  console.log(name);
-  console.log(address);
-  console.log(city, state, zip);
+//   console.log(name);
+//   console.log(address);
+//   console.log(city, state, zip);
+  console.log(`
+${name}
+${address}
+${city}, ${state}, ${zip}
+  `)
 }
 
 function addNumbers(num1, num2) {
@@ -19,35 +24,41 @@ function displayReceipt(totalDue, amountPaid) {
     console.log(`Change Due: $${changeDue}`);
   }
 }
-let name = "Yonesh";
-let address = "123 45st";
-let city = "New York City";
-let state = "NY";
-let zip = 12345;
+// let name = "Yonesh";
+// let address = "123 45st";
+// let city = "New York City";
+// let state = "NY";
+// let zip = 12345;
 
-displayMailingLabel(name, address, city, state, zip);
+// displayMailingLabel(name, address, city, state, zip);
 
-name = "Yonesh";
-address = "123 45st";
-city = "New York City";
-state = "NY";
-zip = 12345;
+displayMailingLabel("Yonesh", "12 45st", "New York City", "NY", 12345);
+// name = "Yonesh";
+// address = "123 45st";
+// city = "New York City";
+// state = "NY";
+// zip = 12345;
 
-displayMailingLabel(name, address, city, state, zip);
+// displayMailingLabel(name, address, city, state, zip);
+displayMailingLabel("Yonesh", "123 45st", "New York City", "NY", 12345);
 
-let num1 = 2;
-let num2 = 4;
 
-addNumbers(num1, num2);
+// let num1 = 2;
+// let num2 = 4;
 
-num1 = 5;
-num2 = 9;
+// addNumbers(num1, num2);
+addNumbers(2,4);
 
-addNumbers(num1, num2);
+// num1 = 5;
+// num2 = 9;
 
-let totalDue = 80;
-let amountPaid = 100;
-displayReceipt(totalDue, amountPaid);
+// addNumbers(num1, num2);
+addNumbers(5,9);
+
+// let totalDue = 80;
+// let amountPaid = 100;
+// displayReceipt(totalDue, amountPaid);
+displayReceipt(80, 100);
 
 totalDue = 100;
 amountPaid = 100;
