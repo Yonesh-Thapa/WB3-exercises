829ffe037b48686dfa2016d7f1b1d2b88601e783
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-01 23:58:09 -0400

    found solution to separating string

--- Strings/advanced_parsing.js
+++ Strings/advanced_parsing.js
@@ -1,15 +1,26 @@
 "use strict";
-
 function parseAndDisplayName(name){
-    let space = name.indexOf(" ");
-    let first = name.substring(0, space);
-    let space2= first.indexOf(" ");
-    let second = name.substring (0, space);
-    let space3 = second.indexOf(" ");
-    let last = name.substring(space3 + 1);
-    console.log(first);
-    console.log(second);
-    console.log(last);
+    let space1 = name.indexOf(" ");
+    let space2 = name.lastIndexOf(" ");
+    if (space1 == -1 && space2 == -1)
+    {
+        console.log(`Name: ${name}`);
+        console.log(`Only name: ${name}`);
+    }
+    else if (space1 != -1  && space2 == -1)
+    {
+        console.log(`Name: ${name}`);
+        console.log(`First name: ${name.substring(0, space1)}`)
+        console.log(`Last namme: ${name.substing(space1)}`);
+    }
+    else if (space1 != -1  && space2 != -1) {
+        console.log(`Name: ${name}`);
+        console.log(`First name: ${name.substring(0, space1)}`)
+        console.log(`Middle name: ${name.substring(space1, space2)}`)
+        console.log(`Last name: ${name.substring(space2)}`);
+        
+    }
+    
 }
 
 parseAndDisplayName("Cher");
