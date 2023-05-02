63f1facefea0f5ce8d0195629e368fb183c59773
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-02 11:40:23 -0400

    global replacement

--- Strings/replace_strings.js
+++ Strings/replace_strings.js
@@ -1,6 +1,19 @@
-let message = "Our corporate offices are located in Dallas.";
+// replace only the first match it finds.
+
+
+let message = "Our corporate offices are located in Dallas. Dallas is awesome.";
 function replaceText(){
-    console.log(message.replace(/DaLLas/i,"Austin"));
+    console.log(message.replace(/DaLLas/i,"Austin"));// /i for insensistive string
+}
+
+replaceText();
+
+// replaces everymatch it finds
+
+message = "Our corporate offices are located in Dallas. Dallas is awesome.";
+function replaceText(){
+    console.log(message.replace(/DaLLas/g,"Austin")); //g for every match it finds
+
 }
 
 replaceText();
