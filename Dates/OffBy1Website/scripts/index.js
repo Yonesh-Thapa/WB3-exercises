29ff5d91a88ff9acb2e662b029abbccec5e60afd
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-02 15:00:04 -0400

    date to string

--- Dates/OffBy1Website/scripts/index.js
+++ Dates/OffBy1Website/scripts/index.js
@@ -10,7 +10,7 @@ function dateDisplay(){
     let now = new Date();
     let time = now.toLocaleTimeString();
     let d = new Date(dateEntered.value + " " + time);
-    document.getElementById("demo").innerHTML = `The date you entered is ${d.toLocaleString()}`;
+    document.getElementById("demo").innerHTML = `The date you entered is ${d}`;
 }
 
 
