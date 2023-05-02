46ef5d1c219d88d3e1d609b44280faafc95cdb4e
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-02 10:15:40 -0400

    rt

--- Strings/part_codes.js
+++ Strings/part_codes.js
@@ -1,26 +1,22 @@
 "use strict";
-function productCode(code){
-
-    let character1 = code.indexOf(":");
-    let character2 = code.indexOf("-");
-    function getSupplier(code){
-        let supplierCode = code.substring(0, character1);
-        return supplierCode;
-
-    }
-    function getProductNumber(code){
-        let productNumber = code.substring(character1 + 1, character2);
-        return productNumber;
-
-    }
-    function getSize(code){
-        let size = code.substring(character2 + 1);
-        return size;
-
-    }
-    console.log(`Supplier Code: ${getSupplier(code)} 
+function productCode(code) {
+  let character1 = code.indexOf(":");
+  let character2 = code.indexOf("-");
+  function getSupplier(code) {
+    let supplierCode = code.substring(0, character1);
+    return supplierCode;
+  }
+  function getProductNumber(code) {
+    let productNumber = code.substring(character1 + 1, character2);
+    return productNumber;
+  }
+  function getSize(code) {
+    let size = code.substring(character2 + 1);
+    return size;
+  }
+  console.log(`Supplier Code: ${getSupplier(code)} 
 Product Number: ${getProductNumber(code)}
-Size: ${getSize(code)}`)
+Size: ${getSize(code)}`);
 }
 
 productCode("ACME:123-L");
