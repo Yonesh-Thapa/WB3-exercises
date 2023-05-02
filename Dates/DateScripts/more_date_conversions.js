a9b1b48f510252d62a444e4a163026303a38600b
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-02 15:07:04 -0400

    more date conversion

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let birthday = new Date("10/23/1995");
+let momBirthday = new Date("10/23/1995");
+let DadBirthday = new Date("10/23/1995");
+
+console.log(`My Birthday: ${birthday.toLocaleString()}
+Mom Brithdday: ${momBirthday.toLocaleString()}
+Dad Birthday: ${DadBirthday.toLocaleString()}`);
+
