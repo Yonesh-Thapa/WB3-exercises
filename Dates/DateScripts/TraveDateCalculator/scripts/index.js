18204a7fccaddb474cf01d5a7511472441ab5014
Yonesh Thapa <hsenoy2022@gmail.com>
2023-05-03 09:53:52 -0400

    Travel Date

---
+++ Dates/DateScripts/TraveDateCalculator/scripts/index.js
@@ -0,0 +1,24 @@
+let travelDate = document.getElementById("travelDate");
+const submitBtn = document.getElementById("submitBtn");
+const travelDays = document.getElementById("travelDays");
+
+function message() {
+  let date = new Date();
+  travelDate = new Date(travelDate.value);
+  let timeDiff = travelDate.getTime() - date.getTime();
+  let millisec_per_day = 1000 * 60 * 60 * 24;
+  let numDays = Math.round(timeDiff / millisec_per_day);
+  if (timeDiff < 0) {
+    travelDays.innerHTML = `Sorry your trip has already passed.`;
+  }
+  else if (time = 0){
+    travelDays.innerHTML = `Your trip is today. Have a nice day`;
+  } else {
+    travelDays.innerHTML = `Your trip is in ${numDays} days.`;
+  }
+  return;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  submitBtn.addEventListener("click", message);
+});
